ac2ed42cc5432ec888ebee0eaa8564047069afe3
Joboa <joboa2015@gmail.com>
2023-04-08 17:27:37 -0400

    Add initial setup

---
+++ TicTacToe/Data/DataContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TicTacToe.Models;
+
+namespace TicTacToe.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Move> Moves { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerMove> PlayerMoves { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>()
+               .HasOne(g => g.Player1)
+               .WithMany(p => p.GamesAsPlayer1)
+               .HasForeignKey(g => g.Player1Id)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Game>()
+               .HasOne(g => g.Player2)
+               .WithMany(p => p.GamesAsPlayer2)
+               .HasForeignKey(g => g.Player2Id)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Game>()
+               .Ignore(g => g.GameBoard)
+               .Property(g => g.GameBoardJson)
+               .HasColumnName("GameBoard");
+
+            modelBuilder.Entity<Player>()
+               .HasMany(p => p.GamesAsPlayer1)
+               .WithOne(g => g.Player1)
+               .HasForeignKey(g => g.Player1Id)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Player>()
+               .HasMany(p => p.GamesAsPlayer2)
+               .WithOne(g => g.Player2)
+               .HasForeignKey(g => g.Player2Id)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Move>()
+               .HasKey(m => new { m.GameId, m.BoardRow, m.BoardColumn });
+        }
+    }
+}
---
+++ TicTacToe/Migrations/20230408212125_Initial.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacToe.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moves",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(type: "int", nullable: false),
+                    BoardRow = table.Column<int>(type: "int", nullable: false),
+                    BoardColumn = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moves", x => new { x.GameId, x.BoardRow, x.BoardColumn });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerMoves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerId = table.Column<int>(type: "int", nullable: false),
+                    GameId = table.Column<int>(type: "int", nullable: false),
+                    BoardRow = table.Column<int>(type: "int", nullable: false),
+                    BoardColumn = table.Column<int>(type: "int", nullable: false),
+                    MoveTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerMoves", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Player1Id = table.Column<int>(type: "int", nullable: false),
+                    Player2Id = table.Column<int>(type: "int", nullable: false),
+                    CurrentPlayer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Winner = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GameBoard = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Players_Player1Id",
+                        column: x => x.Player1Id,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Games_Players_Player2Id",
+                        column: x => x.Player2Id,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Player1Id",
+                table: "Games",
+                column: "Player1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Player2Id",
+                table: "Games",
+                column: "Player2Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Moves");
+
+            migrationBuilder.DropTable(
+                name: "PlayerMoves");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
